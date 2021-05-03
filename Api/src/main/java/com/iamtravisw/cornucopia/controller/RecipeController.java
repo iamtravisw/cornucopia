ebbfe7f5b4dc1873e100b1b18ef906436b87dc0a
Kame <iamtravisw@gmail.com>
2021-05-02 20:44:31 -0600

    add and get recipes finished

--- Api/src/main/java/com/iamtravisw/cornucopia/controller/RecipeController.java
+++ Api/src/main/java/com/iamtravisw/cornucopia/controller/RecipeController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import java.util.Date;
+import java.util.List;
 
 
 @RestController
@@ -20,12 +21,34 @@ public class RecipeController {
     private RecipeRepository recipeRepository;
 
     @PostMapping("/add")
-    public ResponseEntity<?> saveIngredient(@Validated @RequestBody Recipe recipe) {
+    public ResponseEntity<?> saveRecipe(@Validated @RequestBody Recipe recipe) {
         Date date = new Date();
         recipe.setAddDate(date);
         recipe.setModDate(date);
         recipeRepository.save(recipe);
         return ResponseEntity.status(HttpStatus.OK).body(recipe);
+    }
+
+    @GetMapping("/retrieve/{recipeId}")
+    public ResponseEntity<?> getRecipe(@Validated @PathVariable Long recipeId) {
+        Recipe storedRecipe = recipeRepository.findByRecipeId(recipeId);
+        storedRecipe.getUser().setPassword("******");
+        if(storedRecipe.getName() != null){
+            return ResponseEntity.status(HttpStatus.OK).body(storedRecipe);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Cannot find recipe.");
+        }
+    }
+
+    @GetMapping("/retrieve/all/{userId}")
+    public ResponseEntity<?> getAllRecipes(@Validated @PathVariable Long userId) {
+        List<Recipe> storedRecipe = recipeRepository.findAllRecipesByUserId(userId);
+
+        for (Recipe r: storedRecipe) {
+            r.getUser().setPassword("******");
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(storedRecipe);
     }
 
 }
--- Api/src/main/java/com/iamtravisw/cornucopia/repository/RecipeRepository.java
+++ Api/src/main/java/com/iamtravisw/cornucopia/repository/RecipeRepository.java
@@ -10,10 +10,10 @@ import java.util.List;
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-    @Query(value = "SELECT * FROM RECIPIE WHERE INGREDIENT_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM RECIPE WHERE RECIPE_ID = ?1", nativeQuery = true)
     Recipe findByRecipeId(Long recipieId);
 
-    @Query(value = "SELECT * FROM RECIPIE WHERE USER_USER_ID = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM RECIPE WHERE USER_USER_ID = ?1", nativeQuery = true)
     List<Recipe> findAllRecipesByUserId(Long userId);
 
 }
